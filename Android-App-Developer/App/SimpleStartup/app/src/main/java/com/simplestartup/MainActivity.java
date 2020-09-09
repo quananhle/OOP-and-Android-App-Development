e78b4a72f56314749681cbaedd75de853b5b96f2
Quan Le <qle2@hawk.iit.edu>
2020-09-09 16:15:55 -0500

    no message

--- Android-App-Developer/App/SimpleStartup/app/src/main/java/com/simplestartup/MainActivity.java
+++ Android-App-Developer/App/SimpleStartup/app/src/main/java/com/simplestartup/MainActivity.java
@@ -3,12 +3,55 @@ package com.simplestartup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTag";
+    private int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: " + count++ + " **********");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: " + count++ + " ************");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: " + count++ + " ************");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: " + count++ + " ************");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: " + count++ + " ************");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: " + count++ + " ************");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "ABC123", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: " + count++ + " ************");
     }
 }
