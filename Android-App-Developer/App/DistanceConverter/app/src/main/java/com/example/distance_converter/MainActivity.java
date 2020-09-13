dc0cb454ab2cf6d9f380597b7e6b2d546a1473a2
Quan Le <qle2@hawk.iit.edu>
2020-09-13 00:37:05 -0500

    no message

---
+++ Android-App-Developer/App/DistanceConverter/app/src/main/java/com/example/distance_converter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.distance_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText userText;
+    private TextView output;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind var's to the screen widgets
+        userText = findViewById(R.id.editText);
+        output = findViewById(R.id.textView);
+    }
+    public void buttonClicked(View v) {
+        String text = userText.getText().toString();
+        StringBuilder sb = new StringBuilder(text);
+        if(!text.trim().isEmpty()) {
+            output.setText(sb.reverse());
+        }
+    }
+
+}
