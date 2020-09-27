4dbb4b9e0d4530ef3abc1ddd8dc436b61be8ae8b
Quan Le <qle2@hawk.iit.edu>
2020-09-26 22:06:30 -0500

    no message

--- Android-App-Developer/App/MultiNotesPad/app/src/main/java/com/example/multi_notespad/MainActivity.java
+++ Android-App-Developer/App/MultiNotesPad/app/src/main/java/com/example/multi_notespad/MainActivity.java
@@ -4,7 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -32,6 +42,27 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
     }
     private Notes loadFile() {
-        return null;
+        Log.d(TAG, "loadFile: Loading Multi-Note File");
+        notes = new Notes();
+        try {
+            InputStream is = getApplicationContext().openFileInput("Multi-Note.json");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = readerReadLine()) != null) {
+                sb.append(line);
+            }
+
+            JSONObject jsonObject = new JSONObject(sb.toString());
+            String titleStr = jsonObject.getString("title");
+            String descrStr = jsonObject.getString("description");
+            notes.setTitle(titleStr);
+            notes.setDescription(descrStr);
+        } catch (FileNotFoundException fnfe) {
+            Toast.makeText(this, "No Multi-Note File Present", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return notes;
     }
 }
