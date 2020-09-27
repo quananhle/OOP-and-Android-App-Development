87ef81c751e22693a4b106e36cc39a1d3ddc00ce
Quan Le <qle2@hawk.iit.edu>
2020-09-26 21:25:28 -0500

    no message

--- Android-App-Developer/App/MultiNotesPad/app/src/main/java/com/example/multi_notespad/MainActivity.java
+++ Android-App-Developer/App/MultiNotesPad/app/src/main/java/com/example/multi_notespad/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.multi_notespad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private EditText title;
+    private EditText description;
+    private Notes notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.noteTitle);
+        description = findViewById(R.id.noteDescription);
+        description.setMovementMethod(new ScrollingMovementMethod());
+        description.setTextIsSelectable(true);
     }
 }
