b8be9de17a72cf594db2e86accf879f0aa16f126
Quan Le <qle2@hawk.iit.edu>
2020-09-26 21:20:32 -0500

    no message

--- Android-App-Developer/App/MultiNotesPad/app/src/main/java/com/example/multi_notespad/Notes.java
+++ Android-App-Developer/App/MultiNotesPad/app/src/main/java/com/example/multi_notespad/Notes.java
@@ -1,4 +1,29 @@
 package com.example.multi_notespad;
 
-public class Notes {
+import androidx.annotation.NonNull;
+
+public class Notes{
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String title;
+    private String description;
+
+    @NonNull
+    public String toString() {
+        return title + ": " + description;
+    }
 }
