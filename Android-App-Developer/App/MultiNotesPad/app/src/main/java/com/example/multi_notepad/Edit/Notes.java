52e9563c739eb81d61895c36a4813d3a9427aa83
Quan Le <qle2@hawk.iit.edu>
2020-09-30 14:56:17 -0500

    no message

--- Android-App-Developer/App/MultiNotesPad/app/src/main/java/com/example/multi_notepad/Edit/Notes.java
+++ Android-App-Developer/App/MultiNotesPad/app/src/main/java/com/example/multi_notepad/Edit/Notes.java
@@ -6,11 +6,30 @@ public class Notes implements Serializable {
     private String name;
     private String body;
     private String time;
+    private String mainBody;
+    //default constructor
+    public Notes(){
+        name = "";
+        body = "";
+        time = "";
+        if (body.length() > 80){
+            mainBody = body.substring(0, 79) + "...";
+        }
+        else {
+            mainBody = body;
+        }
+    }
     //non-default constructor
     public Notes(String title, String description, String dateTime) {
         this.name = title;
         this.body = description;
         this.time = dateTime;
+        if (body.length() > 80){
+            mainBody = body.substring(0, 79) + "...";
+        }
+        else {
+            mainBody = body;
+        }
     }
     //accessor and mutator methods
     public String getName() {
@@ -24,11 +43,23 @@ public class Notes implements Serializable {
     }
     public void setBody(String body) {
         this.body = body;
+        if(body.length() > 80){
+            mainBody = body.substring(0, 79) + "...";
+        }
+        else{
+            mainBody = body;
+        }
     }
     public String getTime() {
         return time;
     }
     public void setTime(String time) {
         this.time = time;
+    }
+    public String getMainBody() {
+        return this.mainBody;
+    }
+    public void setMainBody(String mainBody) {
+        this.mainBody = mainBody;
     }
 }
