2bd36dca090a65911b7d8644b815a8ae19fada00
Quan Le <qle2@hawk.iit.edu>
2020-10-12 23:03:01 -0500

    no message

--- Android-App-Developer/App/StockWatch/app/src/main/java/com/example/stockwatch/MainActivity.java
+++ Android-App-Developer/App/StockWatch/app/src/main/java/com/example/stockwatch/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.stockwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView stockData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stockData = findViewById(R.id.stockData);
+
+    }
+    public void getData(View v){
+
     }
 }
