def113bb9a9fbc3c86b4edf8c0aa6138d25ce20f
Quan Le <qle2@hawk.iit.edu>
2020-10-16 19:47:51 -0500

    no message

--- Android-App-Developer/App/MultiNotesApp/app/src/main/java/com/example/multi_notepad/Main/MainActivity.java
+++ Android-App-Developer/App/MultiNotesApp/app/src/main/java/com/example/multi_notepad/Main/MainActivity.java
@@ -156,40 +156,6 @@ public class MainActivity extends AppCompatActivity
                 return super.onOptionsItemSelected(item);
         }
     }
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent receivedData){
-//        super.onActivityResult(requestCode, resultCode, receivedData);
-//
-//        switch(requestCode){
-//            //New Note Created
-//            case 1:
-//                if(resultCode==0){
-//                    String noteTitle = receivedData.getStringExtra("NEW_TITLE");
-//                    String noteText = receivedData.getStringExtra("NEW_DESCRIPTION");
-//                    notesList.add(0, new Notes(noteTitle, noteText, getCurrentTime()));
-//                    getSupportActionBar().setTitle(getString(R.string.app_name) + "(" + notesList.size() + ")");
-//                    noteAdapter.notifyDataSetChanged();
-//                }
-//                break;
-//
-//            //Existing Note Edited
-//            case 2:
-//                if(resultCode==0){
-//                    String noteTitle = receivedData.getStringExtra("UPDATED_TITLE");
-//                    String noteText = receivedData.getStringExtra("UPDATED_DESCRIPTION");
-//                    notesList.remove(currentNote);
-//                    notesList.add(0, new Notes(noteTitle, noteText, getCurrentTime()));
-//                    getSupportActionBar().setTitle(getString(R.string.app_name) + "(" + notesList.size() + ")");
-//                    noteAdapter.notifyDataSetChanged();
-//                }
-//
-//                //Note Wasn't Edited
-//                else if(resultCode==-1){
-//                    currentNote = -1;
-//                }
-//                break;
-//        }
-//    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent receivedData) {
         super.onActivityResult(requestCode, resultCode, receivedData);
@@ -222,7 +188,7 @@ public class MainActivity extends AppCompatActivity
                 break;
         }
     }
-    //====================== *** Helper methods *** ======================//
+    //====================== *** HELPER•METHODS *** ======================//
     private void readFile() {
         try {
             InputStream inputStream = getApplicationContext().openFileInput(getString(R.string.notes_file));
