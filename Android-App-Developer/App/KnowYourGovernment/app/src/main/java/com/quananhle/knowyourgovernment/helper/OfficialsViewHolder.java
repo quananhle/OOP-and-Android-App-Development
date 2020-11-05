0fd017f7e0845275e1848b7e820a2556414ea3f0
Quan Le <qle2@hawk.iit.edu>
2020-11-05 16:55:53 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/OfficialsViewHolder.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/OfficialsViewHolder.java
@@ -1,4 +1,18 @@
 package com.quananhle.knowyourgovernment.helper;
 
-public class OfficialsViewHolder {
+import android.view.View;
+import android.widget.TextView;
+
+import com.quananhle.knowyourgovernment.R;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class OfficialsViewHolder extends RecyclerView.ViewHolder {
+    private TextView office;
+    private TextView officialNameParty;
+    public OfficialsViewHolder (View view){
+        super(view);
+        office = (TextView) view.findViewById(R.id.dialog_office);
+        officialNameParty = (TextView) view.findViewById(R.id.dialog_name_party);
+    }
 }
