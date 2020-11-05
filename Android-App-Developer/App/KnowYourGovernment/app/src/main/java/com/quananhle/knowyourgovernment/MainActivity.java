a81ad284bdb8b4e799005b56087e887cc2684d2a
Quan Le <qle2@hawk.iit.edu>
2020-11-05 14:28:23 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/MainActivity.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/MainActivity.java
@@ -1,16 +1,49 @@
 package com.quananhle.knowyourgovernment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.quananhle.knowyourgovernment.helper.OfficialsAdapter;
+import com.quananhle.knowyourgovernment.helper.OfficialsList;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+    private RecyclerView recyclerView;
+    private List<OfficialsList> officialsList;
+    private OfficialsAdapter officialsAdapter;
+    private static MainActivity mainActivity;
+    private ConnectivityManager connectivityManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem){
+        Intent intent;
+        switch (menuItem.getItemId()){
+            case R.id.search_button:
+                return true;
+            case R.id.about_button:
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 }
---
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/OfficialsList.java
@@ -0,0 +1,4 @@
+package com.quananhle.knowyourgovernment.helper;
+
+public class OfficialsList {
+}
