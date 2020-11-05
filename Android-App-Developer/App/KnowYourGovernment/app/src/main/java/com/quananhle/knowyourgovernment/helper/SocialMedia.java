e57efb2859b38e86d66924305d60df657c5df31d
Quan Le <qle2@hawk.iit.edu>
2020-11-05 15:48:42 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/SocialMedia.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/SocialMedia.java
@@ -1,4 +1,49 @@
 package com.quananhle.knowyourgovernment.helper;
 
-public class SocialMedia {
+import java.io.Serializable;
+
+public class SocialMedia implements Serializable {
+    private String googleAccount;
+    private String facebookAccount;
+    private String twitterAccount;
+    private String youtubeChannel;
+    private static final String DEFAULT_DISPLAY = "DATA NOT FOUND";
+    //default constructor
+    public SocialMedia() {
+        this.googleAccount = DEFAULT_DISPLAY;
+        this.facebookAccount = DEFAULT_DISPLAY;
+        this.twitterAccount = DEFAULT_DISPLAY;
+        this.youtubeChannel = DEFAULT_DISPLAY;
+    }
+    //non-default constructor
+    public SocialMedia(String googleAccount, String facebookAccount, String twitterAccount, String youtubeChannel) {
+        this.googleAccount = googleAccount;
+        this.facebookAccount = facebookAccount;
+        this.twitterAccount = twitterAccount;
+        this.youtubeChannel = youtubeChannel;
+    }
+    public String getGoogleAccount() {
+        return googleAccount;
+    }
+    public void setGoogleAccount(String googleAccount) {
+        this.googleAccount = googleAccount;
+    }
+    public String getFacebookAccount() {
+        return facebookAccount;
+    }
+    public void setFacebookAccount(String facebookAccount) {
+        this.facebookAccount = facebookAccount;
+    }
+    public String getTwitterAccount() {
+        return twitterAccount;
+    }
+    public void setTwitterAccount(String twitterAccount) {
+        this.twitterAccount = twitterAccount;
+    }
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
 }
