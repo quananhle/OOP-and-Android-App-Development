8dbcdf53f67bc9113fc1c6143020d31219e8a9dc
Quan Le <qle2@hawk.iit.edu>
2020-11-09 15:48:52 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/about/AboutActivity.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/about/AboutActivity.java
@@ -1,6 +1,25 @@
 package com.quananhle.knowyourgovernment.about;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.quananhle.knowyourgovernment.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
+    private static final String TAG = "AboutActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+    protected void apiClicked(View view){
+        final String API_URL = "https://developers.google.com/civic-information/";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(API_URL));
+        startActivity(intent);
+    }
 }
