632cd7a8badc25d15a112d60e70cd02ad5036165
Quan Le <qle2@hawk.iit.edu>
2020-11-08 21:30:31 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/MainActivity.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/MainActivity.java
@@ -122,7 +122,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
     }
     @Override
-    
+    public void onRequestPermissionResult(int requestCode, int[] resultCodes, String[] permission){
+
+    }
+
 
 
 
