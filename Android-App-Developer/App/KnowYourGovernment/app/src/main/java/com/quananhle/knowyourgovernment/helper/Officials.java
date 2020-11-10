067916aedf89c4fc97e68a1c5a76fd3b1d0c2973
Quan Le <qle2@hawk.iit.edu>
2020-11-10 00:48:45 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/Officials.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/Officials.java
@@ -13,6 +13,7 @@ public class Officials implements Serializable {
     private String photoUrl;
     private SocialMedia socialMedia;
     private static final String DEFAULT_DISPLAY = "DATA NOT FOUND";
+
     //default constructor
     public Officials() {
         this.office = DEFAULT_DISPLAY;
@@ -24,6 +25,7 @@ public class Officials implements Serializable {
         this.emailAddress = DEFAULT_DISPLAY;
         this.photoUrl = DEFAULT_DISPLAY;
     }
+
     //non-default constructor
     public Officials(String office, String name, String party, String address, String phoneNumber,
                      String url, String emailAddress, String photoUrl, SocialMedia socialMedia) {
@@ -37,60 +39,79 @@ public class Officials implements Serializable {
         this.photoUrl = photoUrl;
         this.socialMedia = socialMedia;
     }
+
     public String getOffice() {
         return office;
     }
+
     public void setOffice(String office) {
         this.office = office;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getParty() {
         return party;
     }
+
     public void setParty(String party) {
         this.party = party;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
     public String getUrl() {
         return url;
     }
+
     public void setUrl(String url) {
         this.url = url;
     }
+
     public String getEmailAddress() {
         return emailAddress;
     }
+
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
+
     public String getPhotoUrl() {
         return photoUrl;
     }
+
     public void setPhotoUrl(String photoUrl) {
         this.photoUrl = photoUrl;
     }
+
     public SocialMedia getSocialMedia() {
         return socialMedia;
     }
+
     public void setSocialMedia(SocialMedia socialMedia) {
         this.socialMedia = socialMedia;
     }
+
     @Override
     public String toString() {
         return "Officials{" +
