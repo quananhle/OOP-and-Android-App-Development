e7c81864e8e99a0eb3b83a36b5761bc11020bc2e
Quan Le <qle2@hawk.iit.edu>
2020-11-10 00:48:23 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/OfficialActivity.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/OfficialActivity.java
@@ -1,4 +1,35 @@
 package com.quananhle.knowyourgovernment.details;
 
-public class OfficialActivity {
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.quananhle.knowyourgovernment.R;
+import com.quananhle.knowyourgovernment.helper.Officials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OfficialActivity extends AppCompatActivity {
+
+    public void photoClicked(View view){
+
+    }
+    public void logoClicked(View view){
+
+    }
+    public void facebookClicked(View view){
+
+    }
+    public void twitterClicked(View view){
+
+    }
+    public void youtubeClicked(View view){
+
+    }
+
+
+
 }
