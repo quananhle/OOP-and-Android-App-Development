ae320f309a8094d36f4f6ebe19a114d0792980ca
Quan Le <qle2@hawk.iit.edu>
2020-11-11 12:33:18 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/PhotoDetailActivity.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/PhotoDetailActivity.java
@@ -1,6 +1,20 @@
 package com.quananhle.knowyourgovernment.details;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.quananhle.knowyourgovernment.helper.Official;
+import com.quananhle.knowyourgovernment.helper.SocialMedia;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class PhotoDetailActivity extends AppCompatActivity {
+    private static final String TAG = "OfficialActivity";
+    private TextView location, office, name, party, addressLine1, addressLine2, addressLine3, phone, email, website;
+    private TextView addressLabel, phoneLabel, emailLabel, websiteLabel;
+    private ImageView profilePhoto, partyLogo, facebookButton, twitterButton, youtubeButton;
+    private Official official;
+    private ConstraintLayout constraintLayout, information;
+    private SocialMedia fbChannel, twChannel, ytChannel;
 }
