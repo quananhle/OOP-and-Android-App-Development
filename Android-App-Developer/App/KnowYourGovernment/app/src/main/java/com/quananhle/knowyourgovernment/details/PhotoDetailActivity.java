a49436eacaecdbdcfe0ff31e00c11cbdbf247714
Quan Le <qle2@hawk.iit.edu>
2020-11-11 12:39:58 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/PhotoDetailActivity.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/PhotoDetailActivity.java
@@ -11,10 +11,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class PhotoDetailActivity extends AppCompatActivity {
     private static final String TAG = "PhotoDetailsActivity";
-    private TextView location, office, name, party, addressLine1, addressLine2, addressLine3, phone, email, website;
-    private TextView addressLabel, phoneLabel, emailLabel, websiteLabel;
-    private ImageView profilePhoto, partyLogo, facebookButton, twitterButton, youtubeButton;
+    private TextView location, office, name, party;
+    private ImageView profilePhoto, partyLogo;
     private Official official;
-    private ConstraintLayout constraintLayout, information;
-    private SocialMedia fbChannel, twChannel, ytChannel;
+    private ConstraintLayout constraintLayout;
 }
