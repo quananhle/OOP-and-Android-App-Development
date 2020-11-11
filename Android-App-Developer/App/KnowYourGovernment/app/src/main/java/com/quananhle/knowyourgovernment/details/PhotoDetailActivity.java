a06259725fcb9c4582b438322aa606f9cefb484f
Quan Le <qle2@hawk.iit.edu>
2020-11-11 12:59:41 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/PhotoDetailActivity.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/PhotoDetailActivity.java
@@ -1,8 +1,11 @@
 package com.quananhle.knowyourgovernment.details;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.quananhle.knowyourgovernment.R;
 import com.quananhle.knowyourgovernment.helper.Official;
 import com.quananhle.knowyourgovernment.helper.SocialMedia;
 
@@ -15,4 +18,37 @@ public class PhotoDetailActivity extends AppCompatActivity {
     private ImageView profilePhoto, partyLogo;
     private Official official;
     private ConstraintLayout constraintLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+        setupComponents();
+        setupLocations();
+        populateData();
+    }
+    protected void setupComponents(){
+        location         = findViewById(R.id.location);
+        office           = findViewById(R.id.title);
+        name             = findViewById(R.id.name);
+        party            = findViewById(R.id.party);
+        profilePhoto     = findViewById(R.id.photo);
+        partyLogo        = findViewById(R.id.logo);
+        constraintLayout = findViewById(R.id.information);
+    }
+    protected void setupLocations(){
+        Intent intent = this.getIntent();
+        if(intent.hasExtra("location"))
+            location.setText(intent.getStringExtra("location"));
+        else
+            location.setText("");
+    }
+    protected void populateData(){
+        Intent intent = this.getIntent();
+        if (intent.hasExtra("official")){
+            official = (Official) getIntent().getSerializableExtra("official");
+            if (official != null){
+                
+            }
+        }
+    }
 }
