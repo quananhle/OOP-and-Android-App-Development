016f9880fca67ca10fe47dc5021921b01dd3c507
Quan Le <qle2@hawk.iit.edu>
2020-11-11 13:23:51 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/SocialMedia.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/helper/SocialMedia.java
@@ -3,38 +3,17 @@ package com.quananhle.knowyourgovernment.helper;
 import java.io.Serializable;
 
 public class SocialMedia implements Serializable {
-    private String facebookAccount;
-    private String twitterAccount;
-    private String youtubeChannel;
-    private static final String DEFAULT_DISPLAY = "DATA NOT FOUND";
-    //default constructor
-    public SocialMedia() {
-        this.facebookAccount = DEFAULT_DISPLAY;
-        this.twitterAccount = DEFAULT_DISPLAY;
-        this.youtubeChannel = DEFAULT_DISPLAY;
+    private String channel;
+    private String account;
+
+    public SocialMedia (String type, String account) {
+        this.channel = type;
+        this.account = account;
     }
-    //non-default constructor
-    public SocialMedia(String facebookAccount, String twitterAccount, String youtubeChannel) {
-        this.facebookAccount = facebookAccount;
-        this.twitterAccount = twitterAccount;
-        this.youtubeChannel = youtubeChannel;
+    public String getType() {
+        return channel;
     }
-    public String getFacebookAccount() {
-        return facebookAccount;
-    }
-    public void setFacebookAccount(String facebookAccount) {
-        this.facebookAccount = facebookAccount;
-    }
-    public String getTwitterAccount() {
-        return twitterAccount;
-    }
-    public void setTwitterAccount(String twitterAccount) {
-        this.twitterAccount = twitterAccount;
-    }
-    public String getYouTubeChannel() {
-        return youtubeChannel;
-    }
-    public void setYouTubeChannel(String youtubeChannel) {
-        this.youtubeChannel = youtubeChannel;
+    public String getAccount() {
+        return account;
     }
 }
