76341e3a2ce0272a2cbe5fda71b54f084d088d5b
Quan Le <qle2@hawk.iit.edu>
2020-11-11 13:23:16 -0600

    no message

--- Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/PhotoDetailActivity.java
+++ Android-App-Developer/App/KnowYourGovernment/app/src/main/java/com/quananhle/knowyourgovernment/details/PhotoDetailActivity.java
@@ -1,13 +1,16 @@
 package com.quananhle.knowyourgovernment.details;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.quananhle.knowyourgovernment.R;
 import com.quananhle.knowyourgovernment.helper.Official;
 import com.quananhle.knowyourgovernment.helper.SocialMedia;
+import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -18,6 +21,9 @@ public class PhotoDetailActivity extends AppCompatActivity {
     private ImageView profilePhoto, partyLogo;
     private Official official;
     private ConstraintLayout constraintLayout;
+
+    public static final String DEM = "democratic";
+    public static final String REP = "republican";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +39,7 @@ public class PhotoDetailActivity extends AppCompatActivity {
         party            = findViewById(R.id.party);
         profilePhoto     = findViewById(R.id.photo);
         partyLogo        = findViewById(R.id.logo);
-        constraintLayout = findViewById(R.id.information);
+        constraintLayout = findViewById(R.id.constraint_layout);
     }
     protected void setupLocations(){
         Intent intent = this.getIntent();
@@ -47,8 +53,76 @@ public class PhotoDetailActivity extends AppCompatActivity {
         if (intent.hasExtra("official")){
             official = (Official) getIntent().getSerializableExtra("official");
             if (official != null){
-                
+                office.setText(official.getOffice());
+                name.setText(official.getName());
+                party.setText(official.getParty());
+                if(official.getParty().trim().toLowerCase().contains(DEM)){
+                    aDonkey();
+                }
+                else if(official.getParty().trim().toLowerCase().contains(REP)){
+                    anElephant();
+                }
+                else {
+                    anIndependent();
+                }
+                loadProfilePhoto(official.getPhotoUrl().trim());
             }
         }
+    }
+    protected void aDonkey(){
+        constraintLayout.setBackgroundResource(R.color.democraticBlue);
+        partyLogo.setImageResource(R.drawable.dem_logo);
+        getWindow().setNavigationBarColor(getColor(R.color.democraticBlue));
+    }
+    protected void anElephant(){
+        constraintLayout.setBackgroundResource(R.color.republicanRed);
+        partyLogo.setImageResource(R.drawable.rep_logo);
+        getWindow().setNavigationBarColor(getColor(R.color.republicanRed));
+    }
+    protected void anIndependent(){
+        constraintLayout.setBackgroundResource(R.color.midnight_black);
+        partyLogo.setImageResource(R.drawable.no_party_logo);
+        getWindow().setNavigationBarColor(getColor(R.color.midnight_black));
+    }
+
+    private void loadProfilePhoto(String URL) {
+        if (isConnected()) {
+            Log.d(TAG, "onCreate: Loading Profile Photo");
+            final String photoUrl = this.getIntent().getStringExtra("photoUrl");
+            Log.d(TAG, "onCreate: Phot Url is " + photoUrl);
+            Picasso picasso = new Picasso.Builder(this).listener(new Picasso.Listener() {
+                @Override
+                public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+                    // Here we try https if the http image attempt failed
+                    final String changedUrl = photoUrl.replace("http:", "https:");
+                    Log.d(TAG, "onImageLoadFailed: AAA");
+                    picasso.get().load(changedUrl)
+                            .error(R.drawable.brokenimage)
+                            .placeholder(R.drawable.placeholder)
+                            .into(profilePhoto);
+
+                }
+            }).build();
+            Log.d(TAG, "onCreate: BBB");
+            picasso.load(photoUrl)
+                    .error(R.drawable.brokenimage)
+                    .placeholder(R.drawable.placeholder)
+                    .into(profilePhoto);
+        }
+        else {
+            profilePhoto.setImageResource(R.drawable.placeholder);
+            showMessage(ERROR_ICON, "NO NETWORK CONNECTION",
+                    "Data cannot be accessed/loaded without an Internet connection");
+        }
+
+
+
+
+
+        Picasso.get()
+                .load(URL)
+                .error(R.drawable.brokenimage)
+                .placeholder(R.drawable.placeholder)
+                .into(profilePhoto);
     }
 }
