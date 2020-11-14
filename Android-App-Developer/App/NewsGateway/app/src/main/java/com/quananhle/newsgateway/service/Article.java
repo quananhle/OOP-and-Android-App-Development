7fa871cd4d79dffc4aa435fc3ea5cf161cca0cd4
Quan Le <qle2@hawk.iit.edu>
2020-11-13 23:29:34 -0600

    no message

--- Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/Article.java
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/Article.java
@@ -1,4 +1,32 @@
 package com.quananhle.newsgateway.service;
 
-public class Article {
+import java.io.Serializable;
+
+public class Article implements Serializable {
+    private String headline;
+    private String author;
+    private String publishingDate;
+    private String url;
+    private String imageUrl;
+    private String content;
+    //default constructor
+    public Article() {
+        //News Article Headline (can be null/blank)
+        this.headline       = "";
+        this.author         = "";
+        this.publishingDate = "";
+        this.url            = "";
+        this.imageUrl       = "";
+        this.content        = "";
+    }
+    //non-default constructor
+    public Article(String headline, String author, String publishingDate, String url, String imageUrl, String content) {
+        this.headline = headline;
+        this.author = author;
+        this.publishingDate = publishingDate;
+        this.url = url;
+        this.imageUrl = imageUrl;
+        this.content = content;
+    }
+
 }
