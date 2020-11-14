aece19e3a2b7d3e4d6a42315c9ed5c80ba6db104
Quan Le <qle2@hawk.iit.edu>
2020-11-14 00:59:38 -0600

    no message

--- Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/Source.java
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/Source.java
@@ -1,4 +1,66 @@
 package com.quananhle.newsgateway.service;
 
 public class Source {
+    private String id;
+    private String company;
+    private String category;
+    private String ranking;
+    private Boolean isGovernmentFunded = true;
+    //default constructor
+    public Source() {
+        id                 = null;
+        company            = null;
+        category           = null;
+        ranking            = null;
+        isGovernmentFunded = true;
+    }
+    //non-default constructor
+    public Source(String id, String company, String category, String ranking, Boolean isGovernmentFunded) {
+        this.id = id;
+        this.company = company;
+        this.category = category;
+        this.ranking = ranking;
+        this.isGovernmentFunded = isGovernmentFunded;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(String ranking) {
+        this.ranking = ranking;
+    }
+
+    public Boolean getGovernmentFunded() {
+        return isGovernmentFunded;
+    }
+
+    public void setGovernmentFunded(Boolean governmentFunded) {
+        isGovernmentFunded = governmentFunded;
+    }
+
 }
