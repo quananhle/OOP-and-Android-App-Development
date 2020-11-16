553a33b734ea21de13ae882108339e0473b33e9e
Quan Le <qle2@hawk.iit.edu>
2020-11-16 15:03:41 -0600

    no message

---
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/ArticleFragment.java
@@ -0,0 +1,9 @@
+package com.quananhle.newsgateway.service;
+
+public class ArticleFragment {
+    private static final String TAG = "ArticleFragment";
+    //default constructor
+    public ArticleFragment() {
+    }
+    
+}
