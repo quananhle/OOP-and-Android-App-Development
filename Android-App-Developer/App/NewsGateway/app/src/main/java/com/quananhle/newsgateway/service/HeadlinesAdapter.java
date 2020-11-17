15559408d67a1ddb4f47e08ea99d0b0d0d09ded9
Quan Le <qle2@hawk.iit.edu>
2020-11-17 00:27:59 -0600

    no message

--- Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/HeadlinesAdapter.java
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/HeadlinesAdapter.java
@@ -1,8 +1,11 @@
 package com.quananhle.newsgateway.service;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.quananhle.newsgateway.MainActivity;
+import com.quananhle.newsgateway.R;
 
 import java.util.ArrayList;
 
@@ -13,15 +16,24 @@ public class HeadlinesAdapter extends RecyclerView.Adapter<HeadlinesViewHolder> 
     private static final String TAG = "HeadlinesAdapter";
     private ArrayList<Article>  articleArrayList;
     private MainActivity mainActivity;
+
+    public HeadlinesAdapter(ArrayList<Article> articleArrayList, MainActivity mainActivity) {
+        this.articleArrayList = articleArrayList;
+        this.mainActivity = mainActivity;
+    }
+
     @NonNull
     @Override
     public HeadlinesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.headline, parent, false);
+        itemView.setOnClickListener(mainActivity);
+        return new HeadlinesViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HeadlinesViewHolder holder, int position) {
-
+        final Article article = articleArrayList.get(position);
+        holder.title.setText(article.getTitle());
     }
 
     @Override
