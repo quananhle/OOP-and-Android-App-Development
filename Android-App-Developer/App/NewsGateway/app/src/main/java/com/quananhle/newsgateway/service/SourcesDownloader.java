aabda4963d853f0c07c8f30521a74025f817f1d7
Quan Le <qle2@hawk.iit.edu>
2020-11-17 01:10:05 -0600

    no message

--- Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/SourcesDownloader.java
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/SourcesDownloader.java
@@ -1,4 +1,24 @@
 package com.quananhle.newsgateway.service;
 
-public class SourcesDownloader {
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+
+import com.quananhle.newsgateway.MainActivity;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SourcesDownloader extends AsyncTask<Void, Void, ArrayList<Source>> {
+    private static final String TAG = "SourcesDownloader";
+    @SuppressLint("StaticFieldLeak")
+    private MainActivity mainActivity;
+    private Map<String, ArrayList<Source>> hashMap = new TreeMap<>();
+    public SourcesDownloader(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+    @Override
+    protected ArrayList<Source> doInBackground(Void... voids) {
+        return null;
+    }
 }
