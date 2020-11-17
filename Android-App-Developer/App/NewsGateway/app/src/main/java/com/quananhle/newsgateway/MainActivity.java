4f0296bb4e279eda80fca906f040df61b24a030b
Quan Le <qle2@hawk.iit.edu>
2020-11-17 00:28:13 -0600

    no message

--- Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/MainActivity.java
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/MainActivity.java
@@ -3,12 +3,23 @@ package com.quananhle.newsgateway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
     }
 }
