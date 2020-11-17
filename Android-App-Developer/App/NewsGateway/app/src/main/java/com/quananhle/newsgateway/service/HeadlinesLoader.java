5a4960b71731c3b3f80896972d9e5d520595aae4
Quan Le <qle2@hawk.iit.edu>
2020-11-17 11:56:26 -0600

    no message

--- Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/HeadlinesLoader.java
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/HeadlinesLoader.java
@@ -1,4 +1,26 @@
 package com.quananhle.newsgateway.service;
 
-public class HeadlinesLoader {
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+
+import com.quananhle.newsgateway.MainActivity;
+
+import java.util.ArrayList;
+
+public class HeadlinesLoader extends AsyncTask<Void, Void, ArrayList<Article>> {
+//    @SuppressLint("StaticFieldLeak");
+    private static final String TAG = "HeadlinesLoader";
+    private MainActivity mainActivity;
+    private ArrayList<Article> articleArrayList;
+    private static final String API_KEY = "d86d5dc5ffaa4f0fa9036ad5c35fb4a1";
+    private static final String DATA_URL = "http://newsapi.org/v2/top-headlines?country=us&apiKey=";
+
+    public HeadlinesLoader(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    protected ArrayList<Article> doInBackground(Void... voids) {
+
+    }
 }
