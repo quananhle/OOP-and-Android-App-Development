f44e28cdf3178a1f6699215ef29bcacde00c1b2b
Quan Le <qle2@hawk.iit.edu>
2020-11-16 18:34:28 -0600

    no message

--- Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/ArticleFragment.java
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/ArticleFragment.java
@@ -1,9 +1,21 @@
 package com.quananhle.newsgateway.service;
 
-public class ArticleFragment {
+import android.view.View;
+
+import com.quananhle.newsgateway.MainActivity;
+
+import androidx.fragment.app.Fragment;
+
+public class ArticleFragment extends Fragment implements
+        View.OnClickListener, View.OnLongClickListener{
     private static final String TAG = "ArticleFragment";
     //default constructor
     public ArticleFragment() {
     }
-    
+    private static final ArticleFragment newInstance(MainActivity mainActivity, String title,
+                                                     String date, String author, String image,
+                                                     String description, int index, int max, String url){
+        
+    }
+
 }
