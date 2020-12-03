7817dae30d3a387fa892d4e12f199af97e617427
Quan Le <qle2@hawk.iit.edu>
2020-12-02 21:45:34 -0600

    no message

--- Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/HeadlinesAdapter.java
+++ Android-App-Developer/App/NewsGateway/app/src/main/java/com/quananhle/newsgateway/service/HeadlinesAdapter.java
@@ -21,6 +21,86 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class HeadlinesAdapter extends RecyclerView.Adapter<HeadlinesViewHolder> {
+
+//    private static final String TAG = "HeadLineAdapter";
+//    private List<Article> headlinesList;
+//    private MainActivity mainActivity;
+//
+//    public HeadlinesAdapter(List<Article> headlinesList, MainActivity mainActivity) {
+//        this.headlinesList = headlinesList;
+//        this.mainActivity = mainActivity;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public HeadlinesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+//    {
+//        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.headline,parent,false);
+//        itemView.setOnClickListener(mainActivity);
+//        return new HeadlinesViewHolder(itemView);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull HeadlinesViewHolder holder, int position)
+//    {
+//        final Article temp = headlinesList.get(position);
+//        holder.title.setText(temp.getTitle());
+//        holder.publishedAt.setText(temp.getPublishingDate());
+//        final ImageView picture = holder.image;
+//        ImageButton share = holder.share;
+//
+//        if (temp.getImageUrl() != null || !temp.getImageUrl().equals("null")){
+//            Log.d(TAG, "onBindViewHolder: (image url) " + temp.getImageUrl());
+//            final String imageUrl = temp.getImageUrl().trim();
+//            Picasso picasso = new Picasso.Builder(mainActivity).listener(new Picasso.Listener() {
+//                @Override
+//                public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+//                    final String securedUrl = imageUrl.replace("http:", "https:");
+//                    picasso.get().load(securedUrl)
+//                            .error(R.drawable.brokenimage)
+//                            .placeholder(R.drawable.placeholder)
+//                            .into(picture);
+//                }
+//            }).build();
+//            picasso.get().load(imageUrl).error(R.drawable.brokenimage)
+//                    .placeholder(R.drawable.placeholder)
+//                    .into(picture);
+//        }
+//        else {
+//            Log.d(TAG, "onBindViewHolder: (null image url)");
+//            picture.setImageResource(R.drawable.placeholder);
+//        }
+//
+//        share.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                if(!isNull(temp.getUrl()))
+//                {
+//                    Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+//                    sharingIntent.setType("text/plain");
+//                    String shareBody = "Checkout this new story from News Gateway:\n" + temp.getUrl();
+//                    sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "New Gateway");
+//                    sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+//                    mainActivity.startActivity(sharingIntent);
+//                }
+//
+//            }
+//        });
+//
+//    }
+//
+//    @Override
+//    public int getItemCount()
+//    {
+//        return headlinesList.size();
+//    }
+//
+//    private boolean isNull(String data)
+//    {
+//        return data == null || data.equals("null");
+//    }
+
     private static final String TAG = "HeadlinesAdapter";
     private ArrayList<Article>  articleArrayList;
     private MainActivity mainActivity;
